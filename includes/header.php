dbc7a203b2ce2aee827b4c4006c11e31246e440b
Eric Paul Jaucian <EricPaul.Jaucian@greymouse.local>
2017-11-14 09:04:40 +1000

    Setup sidebar

--- includes/header.php
+++ includes/header.php
@@ -6,8 +6,9 @@
  <html>  
       <head>  
            <title>Management Information System DSS</title>  
-           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+          
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
-           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>   
+            <link rel="stylesheet" type="text/css" href="css/style.css"> 
       </head> 
  <body>  
+ <?php include 'includes/nav.php';?>

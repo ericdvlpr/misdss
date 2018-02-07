84c60e5a39eab4479ab4c05505f80827e38bc52a
Eric Paul <ericjaucian@gmail.com>
2018-02-07 18:44:41 +1000

    Fixes
    Fixes

--- includes/footer.php
+++ includes/footer.php
@@ -19,4 +19,23 @@
 <script src="js/bootstrap.min.js"></script>
 <script src="js/bootstrap-datepicker1.js"></script>
 <script src="js/bootstrap-select.min.js"></script>
+
 <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="//code.jquery.com/ui/1.11.0/jquery-ui.min.js"></script>
+<script type="text/javascript">
+  $(document).ready(function(){
+
+    $('#date').datepicker().focus(function() {
+        $(".ui-datepicker-prev").remove();
+      });
+     $("#num").keyup(function(){
+       var val = $(this).val();
+       if(val==4){
+       	alert("Value is 4");
+       }else if(val>=4){
+       		alert("Value is not allowed");
+       }
+    });
+  });
+  </script>

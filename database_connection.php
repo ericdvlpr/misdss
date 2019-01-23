af3762a885286843801ce0b31a1f94dee4533d7f
Eric Paul <ericjaucian@gmail.com>
2019-01-23 18:30:03 +0800

    Update Database Connection

--- database_connection.php
+++ database_connection.php
@@ -1,7 +1,7 @@
 <?php
 //database_connection.php
 
-$connect = new PDO('mysql:host=localhost;dbname=db_misdss', 'root', '123456');
+$connect = new PDO('mysql:host=ec2-107-22-238-186.compute-1.amazonaws.com;dbname=dc9f8mgkpa0jsi', 'aygabyyzeffuhq', '7ca7fb3752582bea0df33ecbdccf6dfb208ed85b4c0ee490421ead59aa7ddf1b');
 session_start();
 date_default_timezone_set("Asia/Manila");
 ?>

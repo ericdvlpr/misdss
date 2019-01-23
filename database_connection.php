deb15fd1c6656143a0055ba759d2175d6b978b49
Eric Paul <ericjaucian@gmail.com>
2019-01-23 18:37:33 +0800

    Update Database

--- database_connection.php
+++ database_connection.php
@@ -1,7 +1,9 @@
 <?php
 //database_connection.php
-
-$connect = new PDO('mysql:host=ec2-107-22-238-186.compute-1.amazonaws.com;dbname=dc9f8mgkpa0jsi', 'aygabyyzeffuhq', '7ca7fb3752582bea0df33ecbdccf6dfb208ed85b4c0ee490421ead59aa7ddf1b');
+$servername = 'ec2-107-22-238-186.compute-1.amazonaws.com';
+$username = 'aygabyyzeffuhq';
+$password = '7ca7fb3752582bea0df33ecbdccf6dfb208ed85b4c0ee490421ead59aa7ddf1b';
+$connect = new PDO('mysql:host=$servername;dbname=dc9f8mgkpa0jsi', $username, $password);
 session_start();
 date_default_timezone_set("Asia/Manila");
 ?>
